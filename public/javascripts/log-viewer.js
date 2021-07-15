928ddb602efeff3a7c2fd6d8837cc6b11dd07000
kkwrecks <pomodoromrtmap@gmail.com>
2021-07-15 17:41:29 +0800

    part4

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,12 +1,21 @@
 const connection = new WebSocket("ws://localhost:3000") //3-6 create WS client connection object
 const logWindow = document.querySelector("#log-window") //3-7 get reference to log window
+const filePath = document.getElementById("logFilePath").value //4-3 get logFilePath element
 
+//send msg to web socket
 connection.onopen = () => {
-    connection.send("Hello from the client!") //3-8 send msg to web socket
+    if (filePath) {
+        connection.send(filePath); //4-4send log file path to WS server
+    }
+    // connection.send("Hello from the client!") //3-8 send msg to web socket
 }
 
+//display message in log window
 connection.onmessage = (event) => {
-    logWindow.innerHTML = event.data
+    const logs = event.data.split("\n").join("<hr>");
+    logWindow.innerHTML = logs;
+
+    //logWindow.innerHTML = event.data //3-9 display message in log window
 }
 
 

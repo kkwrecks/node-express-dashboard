1fde70a78311be84709390c6b557a710a4b36f1f
kkwrecks <pomodoromrtmap@gmail.com>
2021-07-15 17:43:41 +0800

    part4

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,7 @@ const { body } = require("express-validator");
 /* GET home page. */
 router.get("/", (req, res, next) => {
   res.render("index", { 
-    title: "Log Dashboard", 
-    logFile: req.query.LogFile //4-1 add logFile query
+    title: "Log Dashboard", logFile: req.query.LogFile //4-1 add logFile query
   });
 });
 

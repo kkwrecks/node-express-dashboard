4ed7aa4752f7e7c2d7c8d82a1a5b4003ff83e636
kkwrecks <pomodoromrtmap@gmail.com>
2021-07-16 01:51:35 +0800

    part6

--- services/settings-service.js
+++ services/settings-service.js
@@ -11,13 +11,24 @@ function getSettings() {
 //6-4
 function writeSettings(newSettings) {
   const settingsJSON = JSON.stringify(newSettings, null, 2); 
-
+  try {
+    fs.writeFileSync(settingsFilePath, settingsJSON); 
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 function getDefaultDir() {
+  const defaultDir = getSettings().defaultDir;
+  return (isValidDir(defaultDir) ? defaultDir : process.cwd);
 }
 
 function isValidDir(dirPath) {
+  try {
+    fs.readdirSync(dirPath);
+    return true;
+  } catch {return false;}
 }
 
 module.exports = {

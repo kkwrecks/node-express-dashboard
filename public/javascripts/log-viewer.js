ed1c177a5b628350aa3802b5c5cceb7cdb43c1c1
kkwrecks <pomodoromrtmap@gmail.com>
2021-07-15 16:29:56 +0800

    part3-2

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,5 +1,13 @@
 const connection = new WebSocket("ws://localhost:3000") //3-6 create WS client connection object
-const logWindow = document.querySelector(id="log-window") //get ref to log window
+const logWindow = document.querySelector("#log-window") //3-7 get reference to log window
+
+connection.onopen = () => {
+    connection.send("Hello from the client!") //3-8 send msg to web socket
+}
+
+connection.onmessage = (event) => {
+    logWindow.innerHTML = event.data
+}
 
 
 

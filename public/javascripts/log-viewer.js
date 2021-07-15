c87d2a1dec9d75bd2da3204961b72d99202ec724
kkwrecks <pomodoromrtmap@gmail.com>
2021-07-15 16:22:00 +0800

    part3

---
+++ public/javascripts/log-viewer.js
@@ -0,0 +1,5 @@
+const connection = new WebSocket("ws://localhost:3000") //3-6 create WS client connection object
+const logWindow = document.querySelector(id="log-window") //get ref to log window
+
+
+
